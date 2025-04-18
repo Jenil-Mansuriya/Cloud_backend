1d2f0ea00371f50d34af6580263c1cd79169b991
Jenil-Mansuriya <jenilmansuriya2907@gmail.com>
2025-04-19 00:50:05 +0530

    Update cloud_backend.py

--- cloud_backend.py
+++ cloud_backend.py
@@ -115,6 +115,7 @@ def run_arm(code):
     except Exception as e:
         return jsonify({"error": "Unknown error", "details": str(e)}), 500
 
+port = int(os.getenv("PORT",5001))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=port)
